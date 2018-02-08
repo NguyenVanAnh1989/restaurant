cc6e13585783e71f0ddb6d5d221e5c67ef6c89c3
VanAnh1989 <anhnvd00205@fpt.edu.vn>
2018-02-08 22:38:23 +0700

    add Admin Dao And Admin factory

---
+++ src/main/java/com/restaurant/c1603g/DAO/AddminDAO.java
@@ -0,0 +1,41 @@
+package com.restaurant.c1603g.DAO;
+
+import java.util.List;
+
+import com.restaurant.c1603g.DAO.Responsibility.DAOResponsibility;
+import com.restaurant.c1603g.DAO.Responsibility.SqlConnectDAO;
+import com.restaurant.c1603g.Entity.addmin.Addmin;
+
+public class AddminDAO extends SqlConnectDAO implements DAOResponsibility<Addmin>{
+
+	@Override
+	public Addmin getEntityById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Addmin> getALlEntityByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateEntity(Addmin e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteEntity(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String insertEntity(Addmin e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/restaurant/c1603g/Factory/AddminFactory.java
@@ -0,0 +1,40 @@
+package com.restaurant.c1603g.Factory;
+
+import java.util.List;
+
+import com.restaurant.c1603g.Entity.addmin.Addmin;
+import com.restaurant.c1603g.Factory.InterfaceFactory.CrudEntity;
+
+public class AddminFactory implements CrudEntity<Addmin>{
+
+	@Override
+	public Addmin getEntity(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Addmin> getAllEntity(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String insertEntity(Addmin e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateEntity(Addmin e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteEntity(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
