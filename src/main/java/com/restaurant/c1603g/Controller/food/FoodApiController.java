3cd8afa404b95212cec6883b4abdec974bfdffa9
anhnvd00205 <anhnvd0025@fpt.edu.vn>
2018-03-04 22:41:22 +0700

    fix cross origin

--- src/main/java/com/restaurant/c1603g/Controller/food/FoodApiController.java
+++ src/main/java/com/restaurant/c1603g/Controller/food/FoodApiController.java
@@ -3,6 +3,7 @@ package com.restaurant.c1603g.Controller.food;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.restaurant.c1603g.Entity.food.Food;
 import com.restaurant.c1603g.Service.food.FoodService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class FoodApiController {
 
